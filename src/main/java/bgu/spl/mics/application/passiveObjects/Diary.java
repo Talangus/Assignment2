19c9b5bbc25689405ea05f8c392511b6fc17146e
nehamkin <nehamkin@post.bgu.ac.il>
2020-12-06 18:15:42 +0200

    1/12 start

--- src/main/java/bgu/spl/mics/application/passiveObjects/Diary.java
+++ src/main/java/bgu/spl/mics/application/passiveObjects/Diary.java
@@ -1,6 +1,11 @@
 package bgu.spl.mics.application.passiveObjects;
 
 
+import bgu.spl.mics.MicroService;
+
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Passive data-object representing a Diary - in which the flow of the battle is recorded.
  * We are going to compare your recordings with the expected recordings, and make sure that your output makes sense.
@@ -8,5 +13,17 @@ package bgu.spl.mics.application.passiveObjects;
  * Do not add to this class nothing but a single constructor, getters and setters.
  */
 public class Diary {
+    AtomicInteger totalAttacks = new AtomicInteger(0);
+    HashMap<MicroService,Long> attackersFinishTime;
+    HashMap<MicroService,Long> terminationTimes;
+    long R2D2Deactivate;
 
+    public void addMicroserviceTermination(MicroService m){}
+
+    public void incrementTotalAttacks(){
+        int val;
+        do{
+            val=totalAttacks.get();
+        } while(!totalAttacks.compareAndSet(val,val+1));
+    }
 }
