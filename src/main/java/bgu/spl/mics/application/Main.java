eee78c79ba8627e309390be23fabe48622d8e8bd
Talgnus <Talangus@gmail.com>
2020-12-10 16:31:53 +0200

    removed println and unused imports

--- src/main/java/bgu/spl/mics/application/Main.java
+++ src/main/java/bgu/spl/mics/application/Main.java
@@ -1,15 +1,7 @@
 package bgu.spl.mics.application;
 
-import bgu.spl.mics.MessageBus;
-import bgu.spl.mics.MessageBusImpl;
-import bgu.spl.mics.MicroService;
-import bgu.spl.mics.application.misc.Input;
-import bgu.spl.mics.application.misc.Parser;
-import bgu.spl.mics.application.passiveObjects.Diary;
-import bgu.spl.mics.application.passiveObjects.Ewoks;
-import bgu.spl.mics.application.services.*;
-import com.google.gson.*;
 
+import bgu.spl.mics.application.misc.Initializer;
 import java.io.*;
 
 /** This is the Main class of the application. You should parse the input file,
@@ -17,38 +9,9 @@ import java.io.*;
  * In the end, you should output a JSON.
  */
 public class Main {
-	public static void main(String[] args) throws IOException, InterruptedException {
-		Input input=Parser.getInputFromJson(args[0]);
-		MessageBus bus=MessageBusImpl.getInstance();
-		Ewoks ewoks = Ewoks.createInstance(input.getEwoks());
-		Diary diary= Diary.getInstance();
-		MicroService leah = new LeiaMicroservice(input.getAttacks());
-	   	MicroService han = new HanSoloMicroservice();
-		MicroService c3p0 = new C3POMicroservice();
-		MicroService r2D2 = new R2D2Microservice(input.getR2D2());
-		MicroService lando = new LandoMicroservice(input.getLando());
-		Thread threadLeah = new Thread(leah);
-		Thread threadHan= new Thread(han);
-		Thread threadC3P0 = new Thread(c3p0);
-		Thread threadR2D2= new Thread(r2D2);
-		Thread threadLando = new Thread(lando);
-		threadHan.start();
-		threadC3P0.start();
-		threadLeah.start();
-		threadR2D2.start();
-		threadLando.start();
-		try{
-			threadHan.join();
-			threadC3P0.join();
-			threadLando.join();
-			threadLeah.join();
-			threadR2D2.join();
-		}catch (InterruptedException e){}
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		FileWriter writer = new FileWriter("/home/spl211/output.json");
-		gson.toJson(diary,writer);
-		writer.flush();
-		writer.close();
+	public static void main(String[] args) throws IOException {
+		Initializer.runProgram(args[0]);
+		Initializer.createOutput(args[1]);
 	}
 }
 
