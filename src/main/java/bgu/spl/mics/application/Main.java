5453c269ba321b57573b606a9775312dc27d0b49
Tal <Talangus@gmail.com>
2020-12-14 11:50:35 +0200

    fixed deadlocks

--- src/main/java/bgu/spl/mics/application/Main.java
+++ src/main/java/bgu/spl/mics/application/Main.java
@@ -1,18 +1,9 @@
 package bgu.spl.mics.application;
 
-import bgu.spl.mics.MessageBus;
-import bgu.spl.mics.MessageBusImpl;
-import bgu.spl.mics.MicroService;
-import bgu.spl.mics.application.misc.Initializer;
-import bgu.spl.mics.application.misc.Input;
-import bgu.spl.mics.application.misc.Parser;
-import bgu.spl.mics.application.passiveObjects.Diary;
-import bgu.spl.mics.application.passiveObjects.Ewoks;
-import bgu.spl.mics.application.services.*;
-import com.google.gson.*;
 
+import bgu.spl.mics.application.misc.Initializer;
 import java.io.*;
-import java.util.concurrent.atomic.AtomicInteger;
+
 
 /** This is the Main class of the application. You should parse the input file,
  * create the different components of the application, and run the system.
